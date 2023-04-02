93698b3127a61f6995b2cd52ed7aa31bc4d6a176
Yaroslav <dzisyuk.yari@gmail.com>
2023-04-02 12:37:34 +0300

    added main controllers

---
+++ EnjOffer/EnjOffer.UI/Controllers/LibraryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnjOffer.UI.Controllers
+{
+    public class LibraryController : Controller
+    {
+        [Route("/library")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EnjOffer/EnjOffer.UI/Controllers/WordTrainerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnjOffer.UI.Controllers
+{
+    public class WordTrainerController : Controller
+    {
+        [Route("word-trainer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
