bc21e5f62c70ee11283d465111870437a927f0bc
Yaroslav <dzisyuk.yari@gmail.com>
2023-04-05 02:04:36 +0300

    added unit-tests

--- EnjOffer/UnitTests/UserWordsServiceTest.cs
+++ EnjOffer/UnitTests/UserWordsServiceTest.cs
@@ -13,10 +13,13 @@ namespace UnitTests
     public class UserWordsServiceTest
     {
         private readonly IUserWordsService _userWordsService;
+        private readonly IUsersService _usersService;
 
+        //TO-DO: Avoid the Service Locator Anti-Pattern here. Use Dependency Injection
         public UserWordsServiceTest()
         {
             _userWordsService = new UserWordsService();
+            _usersService = new UsersService();
         }
 
         #region AddUserWord
@@ -35,7 +38,7 @@ namespace UnitTests
         }
 
         [Fact]
-        public void AddUserWord_WordDetailsIsNull()
+        public void AddUserWord_ProperDetails()
         {
             //Arrange
             UserWordsAddRequest? userWordAddRequest = new UserWordsAddRequest()
@@ -85,5 +88,188 @@ namespace UnitTests
 
         #endregion
 
+        #region GetPriority
+
+        [Fact]
+        public void GetPriority_NullDate()
+        {
+            //Arrange
+            UserWordsAddRequest userWordRequest = new UserWordsAddRequest()
+            {
+                Word = "Something",
+                WordTranslation = "Щось",
+                UserId = Guid.NewGuid()
+            };
+            
+            //Act
+            UserWordsResponse userWord_from_add = _userWordsService.AddUserWord(userWordRequest);
+
+            //Assert
+            Assert.Equal(1, userWord_from_add.Priority);
+        }
+
+        #endregion
+
+        #region GetUserWordById
+
+        [Fact]
+        public void GetUserWordById_NullId()
+        {
+            //Arrange
+            Guid? userWordId = null;
+
+            //Act
+            UserWordsResponse? userWord_response_from_get = _userWordsService.GetUserWordById(userWordId);
+
+            //Assert
+            Assert.Null(userWord_response_from_get);
+        }
+
+        [Fact]
+        public void GetUserWordById_ProperId()
+        {
+            //Arrange
+            UserAddRequest user_request = new UserAddRequest()
+            {
+                Email = "example@example.com",
+                Password = "password",
+                Role = EnjOffer.Core.Enums.UserRole.Admin
+            };
+            UserResponse user_response = _usersService.AddUser(user_request);
+
+            //Act
+            UserWordsAddRequest userWord_request = new UserWordsAddRequest()
+            {
+                Word = "Something",
+                WordTranslation = "Щось",
+                UserId = user_response.UserId
+            };
+            UserWordsResponse userWord_response_from_add = _userWordsService.AddUserWord(userWord_request);
+            UserWordsResponse? userWord_response_from_get = _userWordsService.GetUserWordById(userWord_response_from_add.UserWordId);
+
+            //Assert
+            Assert.Equal(userWord_response_from_add, userWord_response_from_get);
+        }
+
+
+        #endregion
+
+        #region GetAllUserWords
+
+        [Fact]
+        public void GetAllUsertWords_EmptyList()
+        {
+            //Act
+            List<UserWordsResponse> actual_userWords_response_list = _userWordsService.GetAllUserWords();
+
+            //Assert
+            Assert.Empty(actual_userWords_response_list);
+        }
+
+        [Fact]
+        public void GetAllUserWords_AddFewUserWords()
+        {
+            //Arrange
+            UserAddRequest userAdd_request1 = new UserAddRequest()
+            {
+                Email = "example@example.com",
+                Password = "password",
+                Role = EnjOffer.Core.Enums.UserRole.Admin
+            };
+            UserAddRequest userAdd_request2 = new UserAddRequest()
+            {
+                Email = "example1@example.com",
+                Password = "password",
+                Role = EnjOffer.Core.Enums.UserRole.Admin
+            };
+
+            UserResponse user_response1 = _usersService.AddUser(userAdd_request1);
+            UserResponse user_response2 = _usersService.AddUser(userAdd_request2);
+
+
+            UserWordsAddRequest userWord_request1 = new UserWordsAddRequest() 
+            { 
+                Word = "Something",
+                WordTranslation = "Щось",
+                UserId = user_response1.UserId
+            };
+            UserWordsAddRequest userWord_request2 = new UserWordsAddRequest() 
+            {
+                Word = "Someone",
+                WordTranslation = "Хтось",
+                UserId = user_response2.UserId
+            };
+            UserWordsAddRequest userWord_request3 = new UserWordsAddRequest()
+            {
+                Word = "Something",
+                WordTranslation = "Щось",
+                UserId = user_response2.UserId
+            };
+
+            List<UserWordsAddRequest> userWords_requests = new List<UserWordsAddRequest>()
+            {
+                userWord_request1, userWord_request2, userWord_request3
+            };
+
+            List<UserWordsResponse> userWords_response_list_from_add = new List<UserWordsResponse>();
+
+            foreach (UserWordsAddRequest userWord in userWords_requests)
+            {
+                UserWordsResponse userWord_response = _userWordsService.AddUserWord(userWord);
+                userWords_response_list_from_add.Add(userWord_response);
+            }
+
+            //Act
+            List<UserWordsResponse> userWords_from_get = _userWordsService.GetAllUserWords();
+
+            //Assert
+            foreach (UserWordsResponse userWord_response_from_add in userWords_response_list_from_add)
+            {
+                Assert.Contains(userWord_response_from_add, userWords_from_get);
+            }
+        }
+
+        #endregion
+
+        #region DeleteDefaultWord
+
+        [Fact]
+        public void DeleteUserWord_ValidId()
+        {
+            //Arrange
+            UserAddRequest user_add_request = new UserAddRequest()
+            {
+                Email = "example@example.com",
+                Password = "Password",
+                Role = EnjOffer.Core.Enums.UserRole.Admin
+            };
+            UserResponse user_response = _usersService.AddUser(user_add_request);
+
+            UserWordsAddRequest userWord_add_request = new UserWordsAddRequest()
+            {
+                Word = "Something",
+                WordTranslation = "Щось",
+                UserId = user_response.UserId
+            };
+            UserWordsResponse userWord_response_from_add = _userWordsService.AddUserWord(userWord_add_request);
+
+            //Act
+            bool isDeleted = _userWordsService.DeleteUserWord(userWord_response_from_add.UserWordId);
+
+            //Assert
+            Assert.True(isDeleted);
+        }
+
+        [Fact]
+        public void DeleteUserWord_InvalidId()
+        {
+            //Act
+            bool isDeleted = _userWordsService.DeleteUserWord(Guid.NewGuid());
+
+            //Assert
+            Assert.False(isDeleted);
+        }
+
+        #endregion
     }
 }
